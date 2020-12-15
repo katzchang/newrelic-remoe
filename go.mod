e7831776f74688cd9f3f33d607584e46660f1439
Kazunori Otani <katzchang@gmail.com>
2020-12-16 02:25:50 +0900

    post new relic metric

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/katzchang/newrelic-remoe
 
 go 1.13
 
-require github.com/reeve0930/go-remoe v0.0.0-20200727151137-ff43df9ee17e // indirect
+require (
+	github.com/newrelic/newrelic-telemetry-sdk-go v0.5.0 // indirect
+	github.com/reeve0930/go-remoe v0.0.0-20200727151137-ff43df9ee17e // indirect
+)
--- newrelic-remoe.go
+++ newrelic-remoe.go
@@ -1,15 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"github.com/reeve0930/go-remoe"
+	"github.com/newrelic/newrelic-telemetry-sdk-go/telemetry"
+	"time"
+
 	"os"
 )
 
 func main() {
 	token := os.Getenv("NATURE_API_TOKEN")
-	client := remoe.NewClient(token)
-	data, _ := client.GetRawData()
+	remoClient := remoe.NewClient(token)
+	hervester, _ := telemetry.NewHarvester(telemetry.ConfigAPIKey(os.Getenv("NEW_RELIC_INSERT_API_KEY")))
 
-	fmt.Println(data)
+	data, _ := remoClient.GetRawData()
+	for _, d := range data {
+		hervester.RecordMetric(telemetry.Gauge{
+			Timestamp: time.Now(),
+			Value:     float64(d.MeasuredInstantaneous),
+			Name:      "remoe.MeasuredInstantaneous",
+			Attributes: map[string]interface{}{
+			"modelId": d.ModelID,
+		},
+		})
+	}
+	hervester.HarvestNow(context.Background())
 }
